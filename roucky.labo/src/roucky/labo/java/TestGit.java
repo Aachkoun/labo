f232c7cc79477cdd2cfc4cc1def575d2f6e9b744
roucky74 <aachkoun@gmail.com>
2015-06-08 20:19:19 +0200

    initial commi

---
+++ roucky.labo/src/roucky/labo/java/TestGit.java
@@ -0,0 +1,14 @@
+package roucky.labo.java;
+
+public class TestGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("first test with git and github");
+
+	}
+
+}
